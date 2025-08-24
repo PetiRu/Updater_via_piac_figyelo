ba349c456e033e9b079a9770135b6dbcdc63c5ed
PetiRu <rusznyakpeti2011@gmail.com>
2025-08-24 19:33:19 +0200

    Create Indito.py

---
+++ run/Indito.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+from pathlib import Path
+
+# login.py elérési útja
+login_path = Path(r"C:\Piac_Screener\login.py")
+
+# Ellenőrzés, hogy létezik-e
+if login_path.exists():
+    # Futtatás Python interpreterrel
+    subprocess.run([sys.executable, str(login_path)])
+else:
+    print(f"A login.py fájl nem található itt: {login_path}")
+    input("Nyomj Entert a kilépéshez...")
